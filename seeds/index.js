2a87cd94e76629a96bfe8a4dcb4d74453820e232
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-05 14:11:18 +0900

    シードデータを入れるロジックを作成（実行済）

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const Cafe = require("../models/cafes");
+const cities = require("./cities");
+const { descriptors, places } = require("./seedHelpers");
 
 mongoose.connect('mongodb://localhost:27017/cafe')
     .then(() => {
@@ -9,3 +12,26 @@ mongoose.connect('mongodb://localhost:27017/cafe')
         console.error(err);
     });
 
+// 配列の中からランダム一個取り出す変数
+const sample = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+};
+
+const randomCity = cities[Math.floor(Math.random() * cities.length)];
+
+const seedDB = async () => {
+    await Cafe.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const cafe = new Cafe({
+            name: `${sample(descriptors)} ${sample.places}`,
+            location: `${randomCity.prefecture} ${randomCity.city}`,
+            description: "最高に良い場所",
+            price: "3000~"
+        });
+        await cafe.save();
+    }
+};
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
