582bd6de2d6050f74584ebbea577691888bb6071
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-05 18:08:16 +0900

    編集機能作成

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
 const Cafe = require("./models/cafes");
+const { name } = require("ejs");
+const methodOverride = require("method-override");
 
 mongoose.connect('mongodb://localhost:27017/cafe')
     .then(() => {
@@ -18,6 +20,8 @@ app.set("views", path.join(__dirname, "views"));
 
 // これがないとres.bodyをsendできない
 app.use(express.urlencoded({extended: true}));
+// これがないと編集と削除リクエストが送れない
+app.use(methodOverride("_method"));
 
 app.get("/cafes", async (req, res) => {
     const cafes = await Cafe.find({});
@@ -37,6 +41,23 @@ app.post("/cafes/new", async (req, res) => {
     });
     await cafe.save();
     res.redirect(`/cafes/${cafe._id}`);
+});
+
+// 編集画面に遷移するためのルーティング
+app.get("/cafes/:id/edit", async (req, res) => {
+    const id = req.params.id;
+    const cafe = await Cafe.findById(id);
+    res.render("cafe/edit", { cafe });
+});
+
+// 編集機能
+app.put("/cafes/:id", async (req, res) => {
+    const id = req.params.id;
+    const cafe = await Cafe.findByIdAndUpdate(id, {
+        name: req.body.cafe.name,
+        price: req.body.cafe.price
+    });
+    res.redirect(`/cafes/${id}`);
 });
 
 // カフェ詳細ページのルーティング
