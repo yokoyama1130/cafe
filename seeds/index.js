abb2c7a3c9dabfa53994d9e0a37dba2998e535e0
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-05 13:38:15 +0900

    seedsデータにDBのサーバー起動ロジック追加

---
+++ seeds/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/cafe')
+    .then(() => {
+        console.log("コネクションOK");
+    })
+    .catch((err) => {
+        console.error("コネクションエラー");
+        console.error(err);
+    });
+
