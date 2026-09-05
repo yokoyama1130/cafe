4ff2f8fd6896e76f1da9b95adaba5e1d4e3db18b
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-05 13:25:32 +0900

    不要な行の削除

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/cafe')
     .then(() => {
         console.log("コネクションOK");
