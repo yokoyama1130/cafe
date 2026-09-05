3b3d52c6c5c53f174f39394fdc7a4c3b4ff5ddf9
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-05 13:19:53 +0900

    カフェ一覧ページの作成とejsの設定

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send("確認");
+    res.render("cafe/index");
 });
 
 app.listen(8080, () => {
