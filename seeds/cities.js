5913dedd8aa71cf99685720587dbbdd8f3933084
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-05 13:35:24 +0900

    シードデータの作成

---
+++ seeds/cities.js
@@ -0,0 +1,93 @@
+module.exports = [
+    {
+        city: "新宿",
+        latitude: 35.6938,
+        longitude: 139.7034,
+        country: "Japan",
+        prefecture: "東京都"
+    },
+    {
+        city: "渋谷",
+        latitude: 35.6580,
+        longitude: 139.7016,
+        country: "Japan",
+        prefecture: "東京都"
+    },
+    {
+        city: "池袋",
+        latitude: 35.7295,
+        longitude: 139.7109,
+        country: "Japan",
+        prefecture: "東京都"
+    },
+    {
+        city: "横浜",
+        latitude: 35.4437,
+        longitude: 139.6380,
+        country: "Japan",
+        prefecture: "神奈川県"
+    },
+    {
+        city: "川崎",
+        latitude: 35.5308,
+        longitude: 139.7029,
+        country: "Japan",
+        prefecture: "神奈川県"
+    },
+    {
+        city: "大阪",
+        latitude: 34.6937,
+        longitude: 135.5023,
+        country: "Japan",
+        prefecture: "大阪府"
+    },
+    {
+        city: "梅田",
+        latitude: 34.7025,
+        longitude: 135.4959,
+        country: "Japan",
+        prefecture: "大阪府"
+    },
+    {
+        city: "京都",
+        latitude: 35.0116,
+        longitude: 135.7681,
+        country: "Japan",
+        prefecture: "京都府"
+    },
+    {
+        city: "名古屋",
+        latitude: 35.1815,
+        longitude: 136.9066,
+        country: "Japan",
+        prefecture: "愛知県"
+    },
+    {
+        city: "札幌",
+        latitude: 43.0618,
+        longitude: 141.3545,
+        country: "Japan",
+        prefecture: "北海道"
+    },
+    {
+        city: "仙台",
+        latitude: 38.2682,
+        longitude: 140.8694,
+        country: "Japan",
+        prefecture: "宮城県"
+    },
+    {
+        city: "福岡",
+        latitude: 33.5902,
+        longitude: 130.4017,
+        country: "Japan",
+        prefecture: "福岡県"
+    },
+    {
+        city: "秋田",
+        latitude: 39.7200,
+        longitude: 140.1026,
+        country: "Japan",
+        prefecture: "秋田県"
+    }
+];
