d04090007f128eeaf5f3188940c87a5ddfae8f43
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-05 13:31:44 +0900

    cafeスキーマ作成

---
+++ models/cafes.js
@@ -0,0 +1,14 @@
+const { name } = require("ejs");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// スキーマ作成
+const cafes = new Schema({
+    name: String,
+    locaion: String,
+    description: String,
+    price: String,
+    image: String
+});
+
+module.exports = mongoose.model("Cafe", cafes);
